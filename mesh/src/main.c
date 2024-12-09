1119c991bba0afebdf05ec8f8f3d83b61fbabc47
“Mikevaku” <mjb.vankuijk@student.avans.nl>
2024-12-09 13:16:17 +0100

    test branch

---
+++ mesh/src/main.c
@@ -0,0 +1,547 @@
+// /* main.c - Application main entry point */
+
+// /*
+//  * Copyright (c) 2017 Intel Corporation
+//  *
+//  * SPDX-License-Identifier: Apache-2.0
+//  */
+
+#include <zephyr/sys/printk.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/hwinfo.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/mesh.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/uart.h>
+#include <string.h>
+
+#include "board.h"
+
+#define OP_ONOFF_GET       BT_MESH_MODEL_OP_2(0x82, 0x01)
+#define OP_ONOFF_SET       BT_MESH_MODEL_OP_2(0x82, 0x02)
+#define OP_ONOFF_SET_UNACK BT_MESH_MODEL_OP_2(0x82, 0x03)
+#define OP_ONOFF_STATUS    BT_MESH_MODEL_OP_2(0x82, 0x04)
+
+#define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
+
+#define MSG_SIZE 32
+
+/* queue to store up to 10 messages (aligned to 4-byte boundary) */
+K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
+
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+static const struct gpio_dt_spec grnLed = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+/* receive buffer used in UART ISR callback */
+static char rx_buf[MSG_SIZE];
+static int rx_buf_pos;
+
+/* Function to initialize GPIO for LED control */
+void led_init(void)
+{
+    if (!device_is_ready(grnLed.port)) {
+        printk("LED device not ready\n");
+        return;
+    }
+    gpio_pin_configure(grnLed.port, grnLed.pin, GPIO_OUTPUT_ACTIVE);
+}
+
+void toggle_led(bool on)
+{
+    gpio_pin_set(grnLed.port, grnLed.pin, on ? 1 : 0);  // Use grnLed.pin
+}
+
+void serial_cb(const struct device *dev, void *user_data)
+{
+	uint8_t c;
+
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
+
+	if (!uart_irq_rx_ready(uart_dev)) {
+		return;
+	}
+
+	/* read until FIFO empty */
+	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+			/* terminate string */
+			rx_buf[rx_buf_pos] = '\0';
+
+			/* if queue is full, message is silently dropped */
+			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+
+			/* reset the buffer (it was copied to the msgq) */
+			rx_buf_pos = 0;
+		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+			rx_buf[rx_buf_pos++] = c;
+		}
+	}
+}
+
+void print_uart(char *buf)
+{
+	int msg_len = strlen(buf);
+
+	for (int i = 0; i < msg_len; i++) {
+		uart_poll_out(uart_dev, buf[i]);
+	}
+}
+
+void uart_init(void)
+{
+    if (!device_is_ready(uart_dev)) {
+        printk("Failed to get UART device binding for %s\n", uart_dev->name);
+        return;
+    }
+
+    printk("UART device %s is ready\n", uart_dev->name);
+
+	int ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+
+	if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled\n");
+		} else if (ret == -ENOSYS) {
+			printk("UART device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART callback: %d\n", ret);
+		}
+		return 0;
+	}
+	uart_irq_rx_enable(uart_dev);
+}
+
+static void attention_on(const struct bt_mesh_model *mod)
+{
+	toggle_led(true);
+}
+
+static void attention_off(const struct bt_mesh_model *mod)
+{
+	toggle_led(false);
+}
+
+static const struct bt_mesh_health_srv_cb health_cb = {
+	.attn_on = attention_on,
+	.attn_off = attention_off,
+};
+
+static struct bt_mesh_health_srv health_srv = {
+	.cb = &health_cb,
+};
+
+BT_MESH_HEALTH_PUB_DEFINE(health_pub, 0);
+
+static const char *const onoff_str[] = { "off", "on" };
+
+static struct {
+	bool val;
+	uint8_t tid;
+	uint16_t src;
+	uint32_t transition_time;
+	struct k_work_delayable work;
+} onoff;
+
+/* OnOff messages' transition time and remaining time fields are encoded as an
+ * 8 bit value with a 6 bit step field and a 2 bit resolution field.
+ * The resolution field maps to:
+ * 0: 100 ms
+ * 1: 1 s
+ * 2: 10 s
+ * 3: 20 min
+ */
+static const uint32_t time_res[] = {
+	100,
+	MSEC_PER_SEC,
+	10 * MSEC_PER_SEC,
+	10 * 60 * MSEC_PER_SEC,
+};
+
+static inline int32_t model_time_decode(uint8_t val)
+{
+	uint8_t resolution = (val >> 6) & BIT_MASK(2);
+	uint8_t steps = val & BIT_MASK(6);
+
+	if (steps == 0x3f) {
+		return SYS_FOREVER_MS;
+	}
+
+	return steps * time_res[resolution];
+}
+
+static inline uint8_t model_time_encode(int32_t ms)
+{
+	if (ms == SYS_FOREVER_MS) {
+		return 0x3f;
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(time_res); i++) {
+		if (ms >= BIT_MASK(6) * time_res[i]) {
+			continue;
+		}
+
+		uint8_t steps = DIV_ROUND_UP(ms, time_res[i]);
+
+		return steps | (i << 6);
+	}
+
+	return 0x3f;
+}
+
+static int onoff_status_send(const struct bt_mesh_model *model,
+			     struct bt_mesh_msg_ctx *ctx)
+{
+	uint32_t remaining;
+
+	BT_MESH_MODEL_BUF_DEFINE(buf, OP_ONOFF_STATUS, 3);
+	bt_mesh_model_msg_init(&buf, OP_ONOFF_STATUS);
+
+	remaining = k_ticks_to_ms_floor32(
+			    k_work_delayable_remaining_get(&onoff.work)) +
+		    onoff.transition_time;
+
+	/* Check using remaining time instead of "work pending" to make the
+	 * onoff status send the right value on instant transitions. As the
+	 * work item is executed in a lower priority than the mesh message
+	 * handler, the work will be pending even on instant transitions.
+	 */
+	if (remaining) {
+		net_buf_simple_add_u8(&buf, !onoff.val);
+		net_buf_simple_add_u8(&buf, onoff.val);
+		net_buf_simple_add_u8(&buf, model_time_encode(remaining));
+	} else {
+		net_buf_simple_add_u8(&buf, onoff.val);
+	}
+
+	return bt_mesh_model_send(model, ctx, &buf, NULL, NULL);
+}
+
+static void onoff_timeout(struct k_work *work)
+{
+	if (onoff.transition_time) {
+		/* Start transition.
+		 *
+		 * The LED should be on as long as the transition is in
+		 * progress, regardless of the target value, according to the
+		 * Bluetooth Mesh Model specification, section 3.1.1.
+		 */
+		toggle_led(true);
+
+		k_work_reschedule(&onoff.work, K_MSEC(onoff.transition_time));
+		onoff.transition_time = 0;
+		return;
+	}
+
+	toggle_led(onoff.val);
+}
+
+/* Generic OnOff Server message handlers */
+
+static int gen_onoff_get(const struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	onoff_status_send(model, ctx);
+	return 0;
+}
+
+static int gen_onoff_set_unack(const struct bt_mesh_model *model,
+			       struct bt_mesh_msg_ctx *ctx,
+			       struct net_buf_simple *buf)
+{
+	uint8_t val = net_buf_simple_pull_u8(buf);
+	uint8_t tid = net_buf_simple_pull_u8(buf);
+	int32_t trans = 0;
+	int32_t delay = 0;
+
+	if (buf->len) {
+		trans = model_time_decode(net_buf_simple_pull_u8(buf));
+		delay = net_buf_simple_pull_u8(buf) * 5;
+	}
+
+	/* Only perform change if the message wasn't a duplicate and the
+	 * value is different.
+	 */
+	if (tid == onoff.tid && ctx->addr == onoff.src) {
+		/* Duplicate */
+		return 0;
+	}
+
+	if (val == onoff.val) {
+		/* No change */
+		return 0;
+	}
+
+	printk("set: %s delay: %d ms time: %d ms\n", onoff_str[val], delay,
+	       trans);
+
+	onoff.tid = tid;
+	onoff.src = ctx->addr;
+	onoff.val = val;
+	onoff.transition_time = trans;
+
+	/* Schedule the next action to happen on the delay, and keep
+	 * transition time stored, so it can be applied in the timeout.
+	 */
+	k_work_reschedule(&onoff.work, K_MSEC(delay));
+
+	return 0;
+}
+
+static int gen_onoff_set(const struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	(void)gen_onoff_set_unack(model, ctx, buf);
+	onoff_status_send(model, ctx);
+
+	return 0;
+}
+
+static const struct bt_mesh_model_op gen_onoff_srv_op[] = {
+	{ OP_ONOFF_GET,       BT_MESH_LEN_EXACT(0), gen_onoff_get },
+	{ OP_ONOFF_SET,       BT_MESH_LEN_MIN(2),   gen_onoff_set },
+	{ OP_ONOFF_SET_UNACK, BT_MESH_LEN_MIN(2),   gen_onoff_set_unack },
+	BT_MESH_MODEL_OP_END,
+};
+
+/* Generic OnOff Client */
+
+static int gen_onoff_status(const struct bt_mesh_model *model,
+			    struct bt_mesh_msg_ctx *ctx,
+			    struct net_buf_simple *buf)
+{
+	uint8_t present = net_buf_simple_pull_u8(buf);
+
+	if (buf->len) {
+		uint8_t target = net_buf_simple_pull_u8(buf);
+		int32_t remaining_time =
+			model_time_decode(net_buf_simple_pull_u8(buf));
+
+		printk("OnOff status: %s -> %s: (%d ms)\n", onoff_str[present],
+		       onoff_str[target], remaining_time);
+		return 0;
+	}
+
+	printk("OnOff status: %s\n", onoff_str[present]);
+
+	return 0;
+}
+
+static const struct bt_mesh_model_op gen_onoff_cli_op[] = {
+	{OP_ONOFF_STATUS, BT_MESH_LEN_MIN(1), gen_onoff_status},
+	BT_MESH_MODEL_OP_END,
+};
+
+/* This application only needs one element to contain its models */
+static const struct bt_mesh_model models[] = {
+	BT_MESH_MODEL_CFG_SRV,
+	BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub),
+	BT_MESH_MODEL(BT_MESH_MODEL_ID_GEN_ONOFF_SRV, gen_onoff_srv_op, NULL,
+		      NULL),
+	BT_MESH_MODEL(BT_MESH_MODEL_ID_GEN_ONOFF_CLI, gen_onoff_cli_op, NULL,
+		      NULL),
+};
+
+static const struct bt_mesh_elem elements[] = {
+	BT_MESH_ELEM(0, models, BT_MESH_MODEL_NONE),
+};
+
+static const struct bt_mesh_comp comp = {
+	.cid = BT_COMP_ID_LF,
+	.elem = elements,
+	.elem_count = ARRAY_SIZE(elements),
+};
+
+/* Provisioning */
+
+static int output_number(bt_mesh_output_action_t action, uint32_t number)
+{
+	printk("OOB Number: %u\n", number);
+
+	board_output_number(action, number);
+
+	return 0;
+}
+
+static void prov_complete(uint16_t net_idx, uint16_t addr)
+{
+	board_prov_complete();
+}
+
+static void prov_reset(void)
+{
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+}
+
+static uint8_t dev_uuid[16];
+
+static const struct bt_mesh_prov prov = {
+	.uuid = dev_uuid,
+	.output_size = 4,
+	.output_actions = BT_MESH_DISPLAY_NUMBER,
+	.output_number = output_number,
+	.complete = prov_complete,
+	.reset = prov_reset,
+};
+
+/** Send an OnOff Set message from the Generic OnOff Client to all nodes. */
+static int gen_onoff_send(bool val)
+{
+	struct bt_mesh_msg_ctx ctx = {
+		.app_idx = models[3].keys[0], /* Use the bound key */
+		.addr = BT_MESH_ADDR_ALL_NODES,
+		.send_ttl = BT_MESH_TTL_DEFAULT,
+	};
+	static uint8_t tid;
+
+	if (ctx.app_idx == BT_MESH_KEY_UNUSED) {
+		printk("The Generic OnOff Client must be bound to a key before "
+		       "sending.\n");
+		return -ENOENT;
+	}
+
+	BT_MESH_MODEL_BUF_DEFINE(buf, OP_ONOFF_SET_UNACK, 2);
+	bt_mesh_model_msg_init(&buf, OP_ONOFF_SET_UNACK);
+	net_buf_simple_add_u8(&buf, val);
+	net_buf_simple_add_u8(&buf, tid++);
+
+	printk("Sending OnOff Set: %s\n", onoff_str[val]);
+
+	return bt_mesh_model_send(&models[3], &ctx, &buf, NULL, NULL);
+}
+
+static void button_pressed(struct k_work *work)
+{
+	if (bt_mesh_is_provisioned()) {
+		(void)gen_onoff_send(!onoff.val);
+		return;
+	}
+
+	/* Self-provision with an arbitrary address.
+	 *
+	 * NOTE: This should never be done in a production environment.
+	 *       Addresses should be assigned by a provisioner, and keys should
+	 *       be generated from true random numbers. It is done in this
+	 *       sample to allow testing without a provisioner.
+	 */
+	static uint8_t net_key[16];
+	static uint8_t dev_key[16];
+	static uint8_t app_key[16];
+	uint16_t addr;
+	int err;
+
+	if (IS_ENABLED(CONFIG_HWINFO)) {
+		addr = sys_get_le16(&dev_uuid[0]) & BIT_MASK(15);
+	} else {
+		addr = k_uptime_get_32() & BIT_MASK(15);
+	}
+
+	printk("Self-provisioning with address 0x%04x\n", addr);
+	err = bt_mesh_provision(net_key, 0, 0, 0, addr, dev_key);
+	if (err) {
+		printk("Provisioning failed (err: %d)\n", err);
+		return;
+	}
+
+	/* Add an application key to both Generic OnOff models: */
+	err = bt_mesh_app_key_add(0, 0, app_key);
+	if (err) {
+		printk("App key add failed (err: %d)\n", err);
+		return;
+	}
+
+	/* Models must be bound to an app key to send and receive messages with
+	 * it:
+	 */
+	models[2].keys[0] = 0;
+	models[3].keys[0] = 0;
+
+	printk("Provisioned and configured!\n");
+}
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_mesh_init(&prov, &comp);
+	if (err) {
+		printk("Initializing mesh failed (err %d)\n", err);
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	/* This will be a no-op if settings_load() loaded provisioning info */
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+
+	printk("Mesh initialized\n");
+}
+
+int main(void)
+{
+	static struct k_work button_work;
+	int err = -1;    
+	char tx_buf[MSG_SIZE];
+
+	printk("Initializing...\n");
+
+	if (IS_ENABLED(CONFIG_HWINFO)) {
+		err = hwinfo_get_device_id(dev_uuid, sizeof(dev_uuid));
+	}
+
+	if (err < 0) {
+		dev_uuid[0] = 0xdd;
+		dev_uuid[1] = 0xdd;
+	}
+
+	printk("Starting UART LED control program\n");
+
+    led_init();  // Initialize LED control
+    uart_init(); // Initialize UART communication
+
+	k_work_init(&button_work, button_pressed);
+
+	err = board_init(&button_work);
+	if (err) {
+		printk("Board init failed (err: %d)\n", err);
+		return 0;
+	}
+
+	k_work_init_delayable(&onoff.work, onoff_timeout);
+
+
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
+
+
+	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
+		/* else: characters beyond buffer size are dropped */
+		if (tx_buf[0] == 'O') {
+			printk("Received 'O' - Turning LED ON\n");
+			toggle_led(true);
+		}   else if (tx_buf[0] == 'F') {
+			printk("Received 'F' - Turning LED OFF\n");
+			toggle_led(false);
+		}
+    }
+
+
+	return 0;
+}
